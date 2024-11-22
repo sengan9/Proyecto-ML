8dbe0b4a76220bc6918c8aec9db0d3852019ebaa
sengan9 <sengan9@gmail.com>
2024-11-22 01:13:23 +0100

    git push

--- SRC/evolution.py
+++ SRC/evolution.py
@@ -14,9 +14,7 @@ from sklearn.ensemble import RandomForestClassifier
 import xgboost as xgb
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, classification_report
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.optimizers import Adam
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV
